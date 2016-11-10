b70d85224a48d421fd7af28fcc9b9b1179482269
Vijay Jegaselvan <vjegase@us.ibm.com>
2016-11-10 12:18:34 -0600

    Format print message.

--- print-message.sh
+++ print-message.sh
@@ -1,4 +1,4 @@
 ENV="`echo $DLMS_SERVER | awk -F  "//" '/1/ {print $2}' | awk -F  "-" '/1/ {print $1}'`"
-echo "##################################################################################################"
-echo "\nData loaded for scenario. Goto https://$ENV-control-center.stage1.ng.bluemix.net and verify results!"
+echo "\n##################################################################################################"
+echo "Data loaded for scenario. Goto https://$ENV-control-center.stage1.ng.bluemix.net and verify results!"
 echo "##################################################################################################"
